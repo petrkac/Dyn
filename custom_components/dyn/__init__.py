ed2f8ba378077f543ccb3cd0d17645a3f09df544
petrkac <96985402+petrkac@users.noreply.github.com>
2025-05-16 22:32:31 +0200

    Update __init__.py

--- custom_components/dyn/__init__.py
+++ custom_components/dyn/__init__.py
@@ -1,6 +1,2 @@
 # Init file for dyn custom component
-def __init__(self, hass, name):
-    self._hass = hass
-    self._name = name
-    self._state = None
-    self._attr_extra_state_attributes = {}
+
